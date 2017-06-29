33fff0f353aa0143e06ca958d745c6c83b1bcaec
dancinturtle <amy.giver@gmail.com>
2017-06-29 14:13:43 -0700

    initial commit

--- WWCTipCalculator/ViewController.swift
+++ WWCTipCalculator/ViewController.swift
@@ -10,15 +10,67 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    var bill: String = "0"
+    var numberBill: Double = 0.00
+    
+    @IBOutlet weak var billDisplay: UILabel!
+    
+    @IBOutlet weak var tenTip: UILabel!
+    @IBOutlet weak var fifteenTip: UILabel!
+    @IBOutlet weak var twentyTip: UILabel!
+    
+    
+    @IBOutlet weak var tenBill: UILabel!
+    @IBOutlet weak var fifteenBill: UILabel!
+    @IBOutlet weak var twentyBill: UILabel!
+    
+    
+    @IBOutlet weak var decimalButton: UIButton!
     
     @IBAction func calculatorButtonWasPressed(_ sender: UIButton) {
         print("button pressed", sender.tag)
+        if(sender.tag >= 0 && sender.tag <= 9){
+            if(bill == "0"){
+                bill = String(sender.tag)
+            }
+            else {
+                bill += String(sender.tag)
+
+            }
+        }
+        else if(sender.tag == 10){
+            bill = "0"
+            decimalButton.isEnabled = true
+        }
+        else if(sender.tag == 11){
+            bill += "."
+            sender.isEnabled = false
+
+        }
+        updateDisplay()
 
     }
     
+    func updateDisplay(){
+        billDisplay.text = bill
+        numberBill = Double(bill)!
+        let tenTipNumber = numberBill * 0.10
+        let fifteenTipNumber = numberBill * 0.15
+        let twentyTipNumber = numberBill * 0.20
+
+        tenTip.text = String(format: "%.2f", tenTipNumber)
+        fifteenTip.text = String(format: "%.2f", fifteenTipNumber)
+        twentyTip.text = String(format: "%.2f", twentyTipNumber)
+        
+        tenBill.text = String(format: "%.2f", numberBill + tenTipNumber)
+        fifteenBill.text = String(format: "%.2f", numberBill + fifteenTipNumber)
+        twentyBill.text = String(format: "%.2f", numberBill + twentyTipNumber)
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        billDisplay.text = bill
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -28,9 +80,12 @@ class ViewController: UIViewController {
     }
 
 
-    @IBAction func mysecondbuttonaction(_ sender: UIButton) {
+}
+
+extension Float {
+    func roundTo(places: Int) -> Float {
+        let divisor = pow(10.0, Float(places))
+        return (self * divisor).rounded() / divisor
     }
-    
-    
 }
 
